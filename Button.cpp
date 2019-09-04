16da39eec4d4d243870057b3678ed5e7afcda184
arthris <oclockth@gmail.com>
2019-09-04 22:15:15 +0700

    Button Part 1 #END

--- Button.cpp
+++ Button.cpp
@@ -1,5 +1,47 @@
-//
-// Created by Paratthakorn Sribunyong on 4/9/2019 AD.
-//
 
 #include "Button.h"
+
+
+
+Button::Button(float x, float y, float width, float height, sf::Font *font, std::string text, sf::Color idleColor,
+               sf::Color hoverColor, sf::Color activeColor)
+               {
+    this->shape.setPosition(sf::Vector2f(x, y));
+    this->shape.setSize(sf::Vector2f(width, height));
+    this->font = font;
+    this->text.setFont(*this->font);
+    this->text.setString(text);
+    this->text.setFillColor(sf::Color::White)
+    this->text.setCharacterSize(12);
+    this->text.setPosition(
+            this->shape.getPosition().x / 2.0f - this->text.getGlobalBounds().width / 2.0f,
+            this->shape.getPosition().y / 2.0f - this->text.getGlobalBounds().height / 2.0f,
+            );
+
+    this->idleColor = idleColor;
+    this->hoverColor = hoverColor;
+    this->activeColor = activeColor;
+
+}
+
+Button::~Button()
+{
+
+
+}
+
+void Button::render(sf::RenderTarget *target) {
+    target->draw(this->shape);
+}
+
+void Button::update(const sf::Vector2f mousePos) {
+    /* Update the bool for Hover and Pressed */
+    if (this->shape.getGlobalBounds().contains(mousePos))
+    {
+
+
+    }
+
+}
+
+
--- Button.h
+++ Button.h
@@ -1,8 +1,34 @@
-//
-// Created by Paratthakorn Sribunyong on 4/9/2019 AD.
-//
-
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
 #ifndef GUJARUNSFML_BUTTON_H
 #define GUJARUNSFML_BUTTON_H
 
+class Button
+{
+public:
+    Button(float x, float y, float width, float height, sf::Font* font,std::string text, sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
+    ~Button();
+
+    //Functions
+    void render(sf::RenderTarget *target);
+    void update(const sf::Vector2f mousePos);
+
+private:
+    sf::RectangleShape shape;
+    sf::Font *font;
+    sf::Text text;
+
+    sf::Color idleColor;
+    sf::Color hoverColor;
+    sf::Color activeColor;
+
+
+
+};
 #endif //GUJARUNSFML_BUTTON_H
